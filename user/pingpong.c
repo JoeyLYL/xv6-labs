850606f392b3968c934ec880ba6da4e0ee42ae13
JoeyLYL <1292094034@qq.com>
2024-10-15 18:44:43 +0800

    完成的pingpong.c

--- user/pingpong.c
+++ user/pingpong.c
@@ -7,23 +7,23 @@ int main(int argc, char *argv[]){
     pipe(par_to_chil);
     pipe(chil_to_par);
     char buf[20] = {0};
+    int pid = 0;
 
     pid = fork();
     if(pid == 0){
         close(par_to_chil[1]);
         close(chil_to_par[0]);
         read(par_to_chil[0], buf, 4);
-        printf("%d, received%s\n", getpid(), buf);
+        printf("%d: received %s\n", getpid(), buf);
         write(chil_to_par[1], "pong", 4);
-        exit(0)
+        exit(0);
     }
     else{
         close(par_to_chil[0]);
         close(chil_to_par[1]);
         write(par_to_chil[1], "ping", 4);
         read(chil_to_par[0], buf, 4);
-        printf("%d, received%s\n", getpid, buf);
-        exit(0)
+        printf("%d: received %s\n", getpid, buf);
+        exit(0);
     }
-    
 }
